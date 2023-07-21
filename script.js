99e8e70970b9164ec40b076fd45551ecf16c28c4
Alexandr Tkachenko <geometrydack@gmail.com>
2023-07-21 10:00:00 +0200

    Grid resizing and badge updates

--- script.js
+++ script.js
@@ -34,3 +34,34 @@ function resetAllSquare() {
   })
 }
 clearBtn.addEventListener("click", resetAllSquare)
+/* Change grid size & badge */
+const gridSizeBar = document.querySelector(".size-adjust #slider")
+function changeGridBadge(squaresNumber) {
+  let gridSizeBadge = document.querySelector(".size-adjust p")
+  gridSizeBadge.innerText = `Grid Size: ${squaresNumber} x ${squaresNumber}`
+}
+function changeGridSize() {
+  resetAllSquare()
+  squaresNumber = parseInt(this.value)
+  if (isNaN(squaresNumber)) {
+    squaresNumber = 30
+    gridSizeBar.value = "30"
+  }
+  changeGridBadge(squaresNumber)
+  grid.style.cssText += `grid-template-columns: repeat(${squaresNumber}, 1fr)`
+  while (squares.length < squaresNumber * squaresNumber - 1) {
+    squares = document.querySelectorAll(".square")
+    createSquare()
+  }
+  while (squares.length > squaresNumber * squaresNumber + 1) {
+    squares = document.querySelectorAll(".square")
+    squares[0].parentNode.removeChild(squares[0])
+  }
+  squares = document.querySelectorAll(".square")
+  if (!gridLines.classList.contains("btn-on")) removeGridLines()
+}
+gridSizeBar.addEventListener("input", (event) => {
+  squaresNumber = event["target"].value
+  changeGridBadge(squaresNumber)
+})
+gridSizeBar.addEventListener("mouseup", changeGridSize)
